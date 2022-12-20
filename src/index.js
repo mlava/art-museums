212733eb39be3f74d02751e320149aa7e0ee73a1
Mark Lavercombe <mlava@users.noreply.github.com>
2022-12-20 16:13:28 +1100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ const config = {
     tabTitle: "Art Museums",
     settings: [
         {
-            id: "met-departments",
-            name: "The Met - Departments",
-            description: "Preferred list of departments within which to search - ",
+            id: "eur-key",
+            name: "Europeana API Key",
+            description: "API Key available from https://pro.europeana.eu/pages/get-api",
             action: { type: "input", placeholder: "Add API key here" },
+        },
+        {
+            id: "eur-language",
+            name: "Europeana preferred language",
+            description: "Two-letter language code from https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes",
+            action: { type: "input", placeholder: "en" },
         },
     ]
 };
@@ -98,6 +104,60 @@ export default {
             }
         });
 
+        window.roamAlphaAPI.ui.commandPalette.addCommand({
+            label: "Random artwork by advanced search (Europeana)",
+            callback: () => {
+                const uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
+                if (uid == undefined) {
+                    alert("Please focus a block before importing an artwork");
+                    return;
+                } else {
+                    window.roamAlphaAPI.updateBlock(
+                        { block: { uid: uid, string: "Loading...".toString(), open: true } });
+                }
+                fetchEurAS().then(async (blocks) => {
+                    if (blocks) {
+                        await window.roamAlphaAPI.updateBlock(
+                            { block: { uid: uid, string: blocks[0].text.toString(), open: true } });
+                        for (var i = 0; i < blocks[0].children.length; i++) {
+                            var thisBlock = window.roamAlphaAPI.util.generateUID();
+                            await window.roamAlphaAPI.createBlock({
+                                location: { "parent-uid": uid, order: i + 1 },
+                                block: { string: blocks[0].children[i].text.toString(), uid: thisBlock }
+                            });
+                        }
+                    }
+                });
+            }
+        });
+
+        window.roamAlphaAPI.ui.commandPalette.addCommand({
+            label: "Random artwork by search term (Europeana)",
+            callback: () => {
+                const uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
+                if (uid == undefined) {
+                    alert("Please focus a block before importing an artwork");
+                    return;
+                } else {
+                    window.roamAlphaAPI.updateBlock(
+                        { block: { uid: uid, string: "Loading...".toString(), open: true } });
+                }
+                fetchEurS().then(async (blocks) => {
+                    if (blocks) {
+                        await window.roamAlphaAPI.updateBlock(
+                            { block: { uid: uid, string: blocks[0].text.toString(), open: true } });
+                        for (var i = 0; i < blocks[0].children.length; i++) {
+                            var thisBlock = window.roamAlphaAPI.util.generateUID();
+                            await window.roamAlphaAPI.createBlock({
+                                location: { "parent-uid": uid, order: i + 1 },
+                                block: { string: blocks[0].children[i].text.toString(), uid: thisBlock }
+                            });
+                        }
+                    }
+                });
+            }
+        });
+
         const args = {
             text: "RANDOMMET",
             help: "Import a Random artwork from The Met",
@@ -113,10 +173,22 @@ export default {
             help: "Import a Random artwork from The Met (by search term)",
             handler: (context) => fetchMetS,
         };
+        const args3 = {
+            text: "RANDOMEURAS",
+            help: "Import a Random artwork from Europeana (Advanced search)",
+            handler: (context) => fetchEurAS,
+        };
+        const args4 = {
+            text: "RANDOMEURS",
+            help: "Import a Random artwork from Europeana (by search term)",
+            handler: (context) => fetchEurAS,
+        };
         if (window.roamjs?.extension?.smartblocks) {
             window.roamjs.extension.smartblocks.registerCommand(args);
             window.roamjs.extension.smartblocks.registerCommand(args1);
             window.roamjs.extension.smartblocks.registerCommand(args2);
+            window.roamjs.extension.smartblocks.registerCommand(args3);
+            window.roamjs.extension.smartblocks.registerCommand(args4);
         } else {
             document.body.addEventListener(
                 `roamjs:smartblocks:loaded`,
@@ -124,9 +196,177 @@ export default {
                     window.roamjs?.extension.smartblocks &&
                     window.roamjs.extension.smartblocks.registerCommand(args) &&
                     window.roamjs.extension.smartblocks.registerCommand(args1) &&
-                    window.roamjs.extension.smartblocks.registerCommand(args2)
+                    window.roamjs.extension.smartblocks.registerCommand(args2) &&
+                    window.roamjs.extension.smartblocks.registerCommand(args3) &&
+                    window.roamjs.extension.smartblocks.registerCommand(args4)
             );
         }
+
+        async function fetchEurS() {
+            var key, eurAPI, eurLang;
+            breakme: {
+                if (!extensionAPI.settings.get("eur-key")) {
+                    key = "API";
+                    sendConfigAlert(key);
+                    break breakme;
+                } else {
+                    eurAPI = extensionAPI.settings.get("eur-key");
+                }
+                if (extensionAPI.settings.get("eur-language")) {
+                    eurLang = extensionAPI.settings.get("eur-language");
+                } else {
+                    eurLang = "en";
+                }
+
+                let string = "What do you want to search for?";
+                searchTerm = await prompt(string, null, 2);
+
+                let url = "https://api.europeana.eu/record/v2/search.json?profile=standard&media=true&rows=1&wskey=" + eurAPI + "&languageCodes=" + eurLang + "&query=" + searchTerm;
+                const response1 = await fetch(url);
+                const data1 = await response1.json();
+                let searchTotal = parseInt(data1.totalResults);
+                if (searchTotal > 999) {
+                    searchTotal = 1000;
+                }
+                let random = randomIntFromInterval(1, searchTotal);
+
+                let url1 = url + "&start=" + random + "";
+                const response = await fetch(url + "&start=" + random + "");
+                if (response.ok) {
+                    const data = await response.json();
+
+                    let title = data.items[0].title[0].toString();
+                    let imageurl = data.items[0].edmIsShownBy[0].toString();
+
+                    let meta = [];
+                    meta.push({ "text": "![](" + imageurl + ")" })
+
+                    if (data.items[0].hasOwnProperty("dcCreator")) {
+                        let artist = data.items[0].dcCreator[0].toString().split(", ");
+                        meta.push({ "text": "Artist:: [[" + artist[1] + " " + artist[0] + "]]" });
+                    }
+                    if (data.items[0].hasOwnProperty("year")) {
+                        meta.push({ "text": "Date:: " + data.items[0].year[0].toString() + "" });
+                    }
+                    /*
+                    if (data.classification != "") {
+                        meta.push({ "text": "Classification:: [[" + data.classification.toString() + "]]" });
+                    }
+                    if (data.period != "") {
+                        meta.push({ "text": "Period:: [[" + data.period.toString() + "]]" });
+                    }
+                    if (data.items[0].dataProvider[0] != "") {
+                        meta.push({ "text": "Repository:: " + data.items[0].dataProvider[0].toString() + "" });
+                    }
+                    if (data.additionalImages.length > 0) {
+                        let imageString = "";
+                        for (var i = 0; i < data.additionalImages.length; i++) {
+                            imageString += "[Image](" + encodeURI(data.additionalImages[i]) + ") "
+                        }
+                        meta.push({ "text": "**Alternative Images:** " + imageString + "" });
+                    }
+                    */
+                    let final = [];
+                    final.push({
+                        text: "**[[" + title + "]]**",
+                        children: meta
+                    });
+                    return final;
+                }
+            }
+        };
+
+        async function fetchEurAS() {
+            var key, eurAPI, eurLang, queryString, title, imageurl;
+            breakme: {
+                if (!extensionAPI.settings.get("eur-key")) {
+                    key = "API";
+                    sendConfigAlert(key);
+                    break breakme;
+                } else {
+                    eurAPI = extensionAPI.settings.get("eur-key");
+                }
+                if (extensionAPI.settings.get("eur-language")) {
+                    eurLang = extensionAPI.settings.get("eur-language");
+                } else {
+                    eurLang = "en";
+                }
+
+                let string = "What do you want to search for?";
+                let selectString = "<select><option value=\"\">Select</option><option value=\"Artist\">Artist</option><option value=\"Location\">Location</option>";
+                selectString += "</select>";
+                searchTerm = await prompt(string, selectString, 4);
+
+                var searchParams = searchTerm.split("-");
+                if (searchParams[1] == "Artist") {
+                    queryString = "&qf=who:" + encodeURIComponent(searchParams[2]) + "";
+                } else if (searchParams[1] == "Location") {
+                    queryString = "&qf=where:" + encodeURIComponent(searchParams[2]) + "";
+                }
+
+                let url = "https://api.europeana.eu/record/v2/search.json?profile=standard&rows=1" + queryString + "&wskey=" + eurAPI + "&languageCodes=" + eurLang + "&query=" + encodeURIComponent(searchParams[0]);
+
+                const response1 = await fetch(url);
+                const data1 = await response1.json();
+                let searchTotal = parseInt(data1.totalResults);
+                if (searchTotal > 999) {
+                    searchTotal = 1000;
+                }
+                let random = randomIntFromInterval(1, searchTotal);
+
+                let url1 = url + "&start=" + random + "";
+                const response = await fetch(url + "&start=" + random + "");
+                if (response.ok) {
+                    const data = await response.json();
+
+                    let meta = [];
+
+                    if (data.items[0].hasOwnProperty("title")) {
+                        title = data.items[0].title[0].toString();
+                    }
+                    if (data.items[0].hasOwnProperty("edmIsShownBy")) {
+                        imageurl = data.items[0].edmIsShownBy[0].toString();
+                        meta.push({ "text": "![](" + imageurl + ")" })
+                    } else if (data.items[0].hasOwnProperty("edmPreview")) {
+                        imageurl = data.items[0].edmPreview[0].toString();
+                        meta.push({ "text": "![](" + imageurl + ")" })
+                    }
+
+
+                    if (data.items[0].hasOwnProperty("dcCreator")) {
+                        let artist = data.items[0].dcCreator[0].toString().split(", ");
+                        meta.push({ "text": "Artist:: [[" + artist[1] + " " + artist[0] + "]]" });
+                    }
+                    if (data.items[0].hasOwnProperty("year")) {
+                        meta.push({ "text": "Date:: " + data.items[0].year[0].toString() + "" });
+                    }
+                    /*
+                    if (data.classification != "") {
+                        meta.push({ "text": "Classification:: [[" + data.classification.toString() + "]]" });
+                    }
+                    if (data.period != "") {
+                        meta.push({ "text": "Period:: [[" + data.period.toString() + "]]" });
+                    }
+                    if (data.items[0].dataProvider[0] != "") {
+                        meta.push({ "text": "Repository:: " + data.items[0].dataProvider[0].toString() + "" });
+                    }
+                    if (data.additionalImages.length > 0) {
+                        let imageString = "";
+                        for (var i = 0; i < data.additionalImages.length; i++) {
+                            imageString += "[Image](" + encodeURI(data.additionalImages[i]) + ") "
+                        }
+                        meta.push({ "text": "**Alternative Images:** " + imageString + "" });
+                    }
+                    */
+                    let final = [];
+                    final.push({
+                        text: "**[[" + title + "]]**",
+                        children: meta
+                    });
+                    return final;
+                }
+            }
+        };
     },
     onunload: () => {
         window.roamAlphaAPI.ui.commandPalette.removeCommand({
@@ -138,10 +378,18 @@ export default {
         window.roamAlphaAPI.ui.commandPalette.removeCommand({
             label: 'Random artwork by search term (The Met)'
         });
+        window.roamAlphaAPI.ui.commandPalette.removeCommand({
+            label: 'Random artwork by advanced search (Europeana)'
+        });
+        window.roamAlphaAPI.ui.commandPalette.removeCommand({
+            label: 'Random artwork by search term (Europeana)'
+        });
         if (window.roamjs?.extension?.smartblocks) {
             window.roamjs.extension.smartblocks.unregisterCommand("RANDOMMET");
             window.roamjs.extension.smartblocks.unregisterCommand("RANDOMMETD");
             window.roamjs.extension.smartblocks.unregisterCommand("RANDOMMETS");
+            window.roamjs.extension.smartblocks.unregisterCommand("RANDOMEURAS");
+            window.roamjs.extension.smartblocks.unregisterCommand("RANDOMEURS");
         }
     }
 }
@@ -278,7 +526,7 @@ async function fetchMetS(searchTerm) {
 
 function sendConfigAlert(key) {
     if (key == "API") {
-        alert("Please set your NASA API key in the configuration settings via the Roam Depot tab.");
+        alert("Please set your API key in the configuration settings via the Roam Depot tab.");
     }
 }
 
@@ -290,7 +538,7 @@ async function prompt(string, selectString, type) {
                 color: 'black',
                 layout: 2,
                 drag: false,
-                timeout: 20000,
+                timeout: false,
                 close: false,
                 overlay: true,
                 title: "Art Museums",
@@ -320,7 +568,7 @@ async function prompt(string, selectString, type) {
                 color: 'black',
                 layout: 2,
                 drag: false,
-                timeout: 20000,
+                timeout: false,
                 close: false,
                 overlay: true,
                 displayMode: 2,
@@ -359,6 +607,87 @@ async function prompt(string, selectString, type) {
                 ],
                 onClosing: function (instance, toast, closedBy) { },
                 onClosed: function (instance, toast, closedBy) { },
+            });
+        })
+    } else if (type == 3) {
+        return new Promise((resolve) => {
+            iziToast.question({
+                theme: 'light',
+                color: 'black',
+                layout: 2,
+                drag: false,
+                timeout: false,
+                close: false,
+                overlay: true,
+                title: "Art Museums",
+                message: string,
+                position: 'center',
+                inputs: [
+                    [
+                        '<input type="text" placeholder="">',
+                        "keyup",
+                        function (instance, toast, input, e) {
+                        },
+                        true,
+                    ],
+                    [selectString, 'change', function (instance, toast, select, e) { }]
+                ],
+                buttons: [
+                    ['<button><b>Confirm</b></button>', function (instance, toast, button, e, inputs) {
+                        instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+                        resolve(inputs[0].value + "-" + inputs[1].options[inputs[1].selectedIndex].value);
+                    }, false], // true to focus
+                    [
+                        "<button>Cancel</button>",
+                        function (instance, toast, button, e) {
+                            instance.hide({ transitionOut: "fadeOut" }, toast, "button");
+                        },
+                    ],
+                ]
+            });
+        })
+    } else if (type == 4) {
+        return new Promise((resolve) => {
+            iziToast.question({
+                theme: 'light',
+                color: 'black',
+                layout: 2,
+                drag: false,
+                timeout: false,
+                close: false,
+                overlay: true,
+                title: "Art Museums",
+                message: string,
+                position: 'center',
+                inputs: [
+                    [
+                        '<input type="text" placeholder="">',
+                        "keyup",
+                        function (instance, toast, input, e) {
+                        },
+                        true,
+                    ],
+                    [selectString, 'change', function (instance, toast, select, e) { }],
+                    [
+                        '<input type="text" placeholder="">',
+                        "keyup",
+                        function (instance, toast, input, e) {
+                        },
+                        false,
+                    ],
+                ],
+                buttons: [
+                    ['<button><b>Confirm</b></button>', function (instance, toast, button, e, inputs) {
+                        instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+                        resolve(inputs[0].value + "-" + inputs[1].options[inputs[1].selectedIndex].value + "-" + inputs[2].value);
+                    }, false], // true to focus
+                    [
+                        "<button>Cancel</button>",
+                        function (instance, toast, button, e) {
+                            instance.hide({ transitionOut: "fadeOut" }, toast, "button");
+                        },
+                    ],
+                ]
             });
         })
     }
